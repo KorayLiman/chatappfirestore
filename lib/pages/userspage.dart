63a5ecbbf84ff7ae3ae605839adf93cec3dfd1eb
Koray Liman <69529444+KorayLiman@users.noreply.github.com>
2022-03-17 15:40:48 +0300

    aaa

--- lib/pages/userspage.dart
+++ lib/pages/userspage.dart
@@ -2,6 +2,7 @@ import 'package:chat/models/usermodel.dart';
 import 'package:chat/pages/konusmapage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 import '../viewmodels/usermodel.dart';
@@ -24,8 +25,8 @@ class _UsersPageState extends State<UsersPage> {
   @override
   void initState() {
     // TODO: implement initState
-    super.initState();
     getUser(_ensongetirilenuser);
+    super.initState();
   }
 
   @override
@@ -36,18 +37,34 @@ class _UsersPageState extends State<UsersPage> {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Users"),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  Navigator.of(context, rootNavigator: true)
-                      .push(MaterialPageRoute(
-                    builder: (context) => Container(),
-                  ));
-                },
-                icon: Icon(Icons.title))
-          ],
         ),
-        body: Container());
+        body: FutureBuilder<List<UserP>>(
+          future: _userModel.getAllUsers(),
+          builder: (context, snapshot) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var oankiuser = snapshot.data![index];
+                return ListTile(
+                  onTap: () {
+                    Navigator.of(context, rootNavigator: true).push(
+                        MaterialPageRoute(
+                            builder: (context) => KonusmaPage(
+                                currentUser: _userModel.user!,
+                                sohbetedilenuser: oankiuser)));
+                  },
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    backgroundImage:
+                        NetworkImage(snapshot.data![index].profileUrl!),
+                  ),
+                  title: Text(snapshot.data![index].userName!),
+                  subtitle: Text(snapshot.data![index].email!),
+                );
+              },
+            );
+          },
+        ));
   }
 
   getUser(UserP? ensongetirilenuser) async {
@@ -66,6 +83,7 @@ class _UsersPageState extends State<UsersPage> {
           .limit(_getirilecekElemanSayisi)
           .get();
     }
+
     for (var snap in querySnapshot.docs) {
       UserP user = UserP.fromMap(snap.data() as Map<String, dynamic>);
       tumkullanicilar.add(user);
